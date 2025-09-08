78cf2091be10f03293bb28446136c290524381b3
therain231 <therain3164@yandex.ru>
2025-09-08 12:08:09 +0300

    set up dev environment

--- SynappsTests/SynappsTests.swift
+++ SynappsTests/SynappsTests.swift
@@ -5,13 +5,11 @@
 //  Created by Andrey Stepanov on 08.09.2025.
 //
 
-import Testing
 @testable import Synapps
+import Testing
 
 struct SynappsTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
 }
