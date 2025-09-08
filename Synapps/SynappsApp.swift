64112740b9952ed4859160f11b956ab0e94e129c
therain231 <therain3164@yandex.ru>
2025-09-08 11:20:28 +0300

    Initial Commit

---
+++ Synapps/SynappsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynappsApp.swift
+//  Synapps
+//
+//  Created by Andrey Stepanov on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SynappsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SynappsTests/SynappsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SynappsTests.swift
+//  SynappsTests
+//
+//  Created by Andrey Stepanov on 08.09.2025.
+//
+
+import Testing
+@testable import Synapps
+
+struct SynappsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
