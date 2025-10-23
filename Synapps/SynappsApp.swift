de8645864981a082c9dd4f097de36e2dd60188c6
therain231 <therain3164@yandex.ru>
2025-10-23 18:20:38 +0300

    vm factory

--- Synapps/SynappsApp.swift
+++ Synapps/SynappsApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct SynappsApp: App {
+  let viewModelFactory: ViewModelFactoryProtocol
+
+  init() {
+    viewModelFactory = MockViewModelFactory()
+  }
+
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ContentView(viewModel: viewModelFactory.createContentViewModel())
+        .environment(\.viewModelFactory, viewModelFactory)
     }
+  }
+}
+
+private struct ViewModelFactoryKey: EnvironmentKey {
+  static let defaultValue: ViewModelFactoryProtocol = MockViewModelFactory()
+}
+
+extension EnvironmentValues {
+  var viewModelFactory: ViewModelFactoryProtocol {
+    get { self[ViewModelFactoryKey.self] }
+    set { self[ViewModelFactoryKey.self] = newValue }
   }
 }
