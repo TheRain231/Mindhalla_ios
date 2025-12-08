1ca802a159f7f56b0fdbc67b4c0dc439601edf8e
therain231 <therain3164@yandex.ru>
2025-12-08 09:04:02 +0300

    viewModelFactory.modelContainer

--- Synapps/Fundamentals/ViewModelFactory/MockViewModelFactory.swift
+++ Synapps/Fundamentals/ViewModelFactory/MockViewModelFactory.swift
@@ -6,10 +6,24 @@
 //
 
 import Foundation
+import SwiftData
 import SwiftUI
 
 final class MockViewModelFactory: ViewModelFactoryProtocol {
   let networkManager: NetworkManagerProtocol = MockNetworkManager()
+  @MainActor // для mainContext
+  let modelContainer: ModelContainer = {
+    let schema = Schema([BookFullResponse.self, BookMetaResponse.self])
+    let cfg = ModelConfiguration(for: BookFullResponse.self, BookMetaResponse.self, isStoredInMemoryOnly: true)
+
+    let container = try! ModelContainer(for: schema, configurations: [cfg])
+
+    for _ in 0..<10 {
+      container.mainContext.insert(BookMetaResponse.mock())
+    }
+
+    return container
+  }()
 
   func createContentViewModel() -> ContentView.ViewModel {
     ContentView.ViewModel()
