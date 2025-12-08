1eb664b8d4b84e0a3ebd810214ccc22dd20732e0
therain231 <therain3164@yandex.ru>
2025-12-08 09:51:51 +0300

    swiftdata to homeView

--- Synapps/Fundamentals/ViewModelFactory/ViewModelFactory.swift
+++ Synapps/Fundamentals/ViewModelFactory/ViewModelFactory.swift
@@ -33,15 +33,18 @@ final class ViewModelFactory: ViewModelFactoryProtocol {
     }()
   }
 
+  @MainActor
   func createContentViewModel() -> ContentView.ViewModel {
     ContentView.ViewModel()
   }
 
+  @MainActor
   func createCardsViewModel(cardID: String) -> CardsView.ViewModel {
     CardsView.ViewModel(cardID: cardID, networkManager: networkManager)
   }
 
+  @MainActor
   func createHomeViewModel() -> HomeView.ViewModel {
-    HomeView.ViewModel(networkManager: networkManager)
+    HomeView.ViewModel(networkManager: networkManager, modelContext: modelContainer.mainContext)
   }
 }
